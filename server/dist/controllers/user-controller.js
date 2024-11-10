dd0458b48f588964a762787f2e173ae25c14a398
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-11-09 20:11:19 -0500

    fixed init

--- server/dist/controllers/user-controller.js
+++ server/dist/controllers/user-controller.js
@@ -40,34 +40,11 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
     const { username, password } = req.body;
     // Find a user in the database by the username provided in req.body
-    const user = await User.findOne({
-        where: {
-            username
-        }
-    });
     // If no user found, send a 403 json response with a user not found message and return
-    if (!user) {
-        res.status(403).json({
-            message: 'No user found with that email address'
-        });
-        return;
-    }
     // If user is found, verify the password is correct (ie. user.validatePassword(password))
-    const valid_pass = await user.validatePassword(password);
     // If password is validated then create a jwt token using the createToken function above and passing their id
-    if (!valid_pass) {
-        res.status(403).json({
-            message: 'Incorrect password'
-        });
-        return;
-    }
-    const token = createToken(user.id);
     // Send a cookie back with the name of token and the token as value. Make sure to set httpOnly in the options object (ie. res.cookie())
-    res.cookie('token', token, {
-        httpOnly: true
-    });
-    // Send a json response back with the user attached
-    res.json(user);
+    // Send the user in a json response - res.json(user)
 };
 // Retrieve a user by their jwt
 // GET /auth/user
--- server/dist/routes/middleware/auth.js
+++ server/dist/routes/middleware/auth.js
@@ -1,28 +1,10 @@
 import jwt from 'jsonwebtoken';
 const { verify } = jwt;
-// Create a middleware function that blocks unauthicated users from triggering a route
+// Create a middleware function that blocks unauthicated users from triggering user ONLY routes
 export const blockGuests = (req, res, next) => {
     // TODO: Retrieve the token cookie from req.cookies
-    const token = req.cookies.token;
-    if (!token) {
-        res.status(401).json({
-            message: 'You are not authorized'
-        });
-        return;
-    }
-    try {
-        const secret = process.env.JWT_SECRET;
-        if (!secret) {
-            throw new Error('JWT_SECRET is not defined');
-        }
-        const userData = verify(token, secret);
-        if (userData && typeof userData !== 'string') {
-            next();
-        }
-    }
-    catch (error) {
-        res.status(401).json({
-            message: 'Not authorized'
-        });
-    }
+    // TODO: If the token cookie does not exist, send a 401 json response message and return
+    // TODO: If the token exists, validate it with the verify function, ( ie. verify(token, process.env.JWT_SECRET) )
+    // TODO: If it verifies, call next to move the request on to the controller function
+    // TODO: If it doesn't verify send a 401 json response message and DO NOT call next
 };
